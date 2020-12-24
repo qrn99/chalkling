21589172cec668dfe88c34a68007c404c4bd67df
Iris-0829 <1648333063@qq.com>
2020-12-24 16:34:52 -0500

    Update LoginWebAPI

--- src/main/java/business_logic_layer/LoginController.java
+++ src/main/java/business_logic_layer/LoginController.java
@@ -2,4 +2,14 @@ package business_logic_layer;
 
 public class LoginController {
 
+  private static LoginController instance = new LoginController();
+
+  public static LoginController getInstance() {
+    return instance;
+  }
+
+  public boolean check(String username, String password) {
+    return false;
+  }
+
 }
--- src/main/java/business_logic_layer/LoginWebAPI.java
+++ src/main/java/business_logic_layer/LoginWebAPI.java
@@ -1,5 +1,40 @@
 package business_logic_layer;
 
-public class LoginWebAPI {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginWebAPI extends HttpServlet {
+
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    return;
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+    String requestUrl = request.getRequestURI();
+    String username = requestUrl.substring("/login/username".length());
+    String password = requestUrl.substring("/login/password".length());
+
+
+    Boolean bool = LoginController.getInstance().check(username, password);
+
+    if (bool){
+      // login credentials are correct
+
+    }
+    else{
+
+    }
+  }
+
+
 
 }
