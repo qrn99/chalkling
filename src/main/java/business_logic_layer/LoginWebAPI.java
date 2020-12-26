a706315b773668466b2387b8d2b17562b681ea09
Iris-0829 <1648333063@qq.com>
2020-12-26 18:25:53 -0500

    Update LoginWebAPI

--- src/main/java/business_logic_layer/LoginWebAPI.java
+++ src/main/java/business_logic_layer/LoginWebAPI.java
@@ -1,35 +1,65 @@
 package business_logic_layer;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+//import database_layer.UserDAC;
 import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-public class LoginWebAPI extends HttpServlet {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+@Controller
+public class LoginWebAPI {
+//  private final UserDAC userDAC;
 
-    String requestUrl = request.getRequestURI();
-    String username = requestUrl.substring("/login/username".length());
-    String password = requestUrl.substring("/login/password".length());
+//  public LoginWebAPI(UserDAC userDAC){
+//    this.userDAC = userDAC;
+//  }
 
+  @RequestMapping("/")
+  String index() {
+    return "index";
+  }
+
+  @RequestMapping("/login")
+  String login(@RequestParam(value="username") String username, @RequestParam(value="password") String password) {
     UserService userService = new UserService();
     LoginController loginController = new LoginController(userService);
     loginController.check(username, password);
-    Boolean bool = loginController.getLoginState();
+    boolean bool = loginController.getLoginState();
 
     String json = "{\n";
     json += "\"username\": " + JSONObject.quote(username) + ",\n";
     json += "\"password\": " + JSONObject.quote(password) + ",\n";
-    json += "\"success\": " + bool + "\n";
+    json += "\"isLogin\": " + bool + "\n";
     json += "}";
-    response.getOutputStream().println(json);
 
+    return json;
   }
 
+//  @RequestMapping("/db")
+//  String db(Map<String, Object> model) {
+//    try (Connection connection = databaseLogic.getDatabase().getConnection()) {
+//      Statement stmt = connection.createStatement();
+//      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+//      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+//      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+//
+//      ArrayList<String> output = new ArrayList<String>();
+//      while (rs.next()) {
+//        output.add("Read from DB: " + rs.getTimestamp("tick"));
+//      }
+//
+//      model.put("records", output);
+//      return "db";
+//    } catch (Exception e) {
+//      model.put("message", e.getMessage());
+//      return "error";
+//    }
+//  }
 }
