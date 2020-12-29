f32f0321099c368a883160c1d1eb5d7831f6c97e
Iris-0829 <1648333063@qq.com>
2020-12-29 16:22:51 -0500

    Automate the server running and merge the test cases

--- src/test/java/LoginWebAPITest.java
+++ src/test/java/LoginWebAPITest.java
@@ -2,6 +2,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -9,12 +11,19 @@ import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 import static org.junit.Assert.*;
 
+import teamchalkling.chalkling.Main;
+
 import java.io.IOException;
 
 
 public class LoginWebAPITest {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Before
+    public void setUp() {
+        Main.main(new String[] {});
+    }
 
 
     @Test
@@ -40,32 +49,28 @@ public class LoginWebAPITest {
         assertNotNull(resultAsJsonStr);
         assertNotNull(root);
         assertEquals("true", root.path("isLogin").asText());
-    }
 
-    @Test
-    public void testIncorrectCredentials() throws IOException, JSONException {
-        // NOTE: It assumes user1 has a password of pass1 and exist in the database.
-        String postUrl = "http://localhost:5000/api/login";
 
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
+//        postUrl = "http://localhost:5000/api/login";
+
+        restTemplate = new RestTemplate();
+        headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        JSONObject loginJsonObject = new JSONObject();
+        loginJsonObject = new JSONObject();
         loginJsonObject.put("username", "user1");
         loginJsonObject.put("password", "pass2");
         loginJsonObject.put("isLogin", "false");
 
-        HttpEntity<String> request =
-                new HttpEntity<String>(loginJsonObject.toString(), headers);
+        request = new HttpEntity<String>(loginJsonObject.toString(), headers);
 
-        String resultAsJsonStr =
-                restTemplate.postForObject(postUrl, request, String.class);
-        JsonNode root = objectMapper.readTree(resultAsJsonStr);
+        resultAsJsonStr = restTemplate.postForObject(postUrl, request, String.class);
+        root = objectMapper.readTree(resultAsJsonStr);
 
         assertNotNull(resultAsJsonStr);
         assertNotNull(root);
         assertEquals("false", root.path("isLogin").asText());
     }
+
 
 }
 
