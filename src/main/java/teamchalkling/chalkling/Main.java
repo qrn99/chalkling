7fcc2db17619f2828ca3f32fd5bbe97e48742593
Iris-0829 <1648333063@qq.com>
2020-12-29 21:01:36 -0500

    Update corsConfigurer

--- src/main/java/teamchalkling/chalkling/Main.java
+++ src/main/java/teamchalkling/chalkling/Main.java
@@ -16,7 +16,7 @@ public class Main {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/login").allowedOrigins("http://localhost:5000");
+                registry.addMapping("/**");
             }
         };
     }
