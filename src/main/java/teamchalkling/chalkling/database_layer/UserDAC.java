b63735c04c2cdbc57e954d67c31ec693480a6fd1
Iris-0829 <1648333063@qq.com>
2020-12-30 20:18:06 -0500

    Fix a bug

--- src/main/java/teamchalkling/chalkling/database_layer/UserDAC.java
+++ src/main/java/teamchalkling/chalkling/database_layer/UserDAC.java
@@ -14,9 +14,9 @@ public class UserDAC extends DAC{
 
     // TODO: Temporary; implement later.
     public void read(){
-        userService.addUser("user1", "salt1", "pass1");
-//        userService.addUser("user2", "pass2");
-//        userService.addUser("user3", "pass3");
+        userService.addUser("user1", "$2a$10$vJ7jWUxKCI7stLVtgtpszO", "$2a$10$vJ7jWUxKCI7stLVtgtpszOnq91hVZttt9TbQ401fjqdz9ct5iW0Ju");
+        userService.addUser("user2", "$2a$10$Fxh.tTBv2V3MikJTIXB2.O", "$2a$10$Fxh.tTBv2V3MikJTIXB2.OlN324mu9cvl.ceKpXwAP/cTFU2Bf3UG");
+        userService.addUser("user3", "$2a$10$5wS1RHT.rFVyhlNowhD/Fu", "$2a$10$5wS1RHT.rFVyhlNowhD/FuX6OHfvgW0lo6AQB4UEmodWPPmFC8qIC");
     }
 
     // TODO: Temporary; implement later.
